6d61a8047ba40427260b9e60b929a6aead455c05
Amit Dahat <50491208+ammy-git@users.noreply.github.com>
2019-10-11 13:13:38 +0530

    edit minu.py

--- minu.py
+++ minu.py
@@ -5,3 +5,24 @@
     sum = float(num1) + float(num2)
     # Display the sum
     print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+#i m editing new code
+# Python Program to find the L.C.M. of two input number
+
+# define a function
+def lcm(x, y):
+   """This function takes two
+   integers and returns the L.C.M."""
+
+   # choose the greater number
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
